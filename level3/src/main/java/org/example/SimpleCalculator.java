be0fcfa57618ec130666ab0a812ae563af7dac01
Abhinav Prasad Singh <a91106283@gmail.com>
2026-03-25 01:09:28 +0530

    added level3 code

---
+++ level3/src/main/java/org/example/SimpleCalculator.java
@@ -0,0 +1,24 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        char operator = '+';
+
+        switch (operator) {
+            case '+':
+                System.out.println("Result = " + (num1 + num2));
+                break;
+            case '-':
+                System.out.println("Result = " + (num1 - num2));
+                break;
+            case '*':
+                System.out.println("Result = " + (num1 * num2));
+                break;
+            case '/':
+                System.out.println("Result = " + (num1 / num2));
+                break;
+            default:
+                System.out.println("Invalid Operator");
+        }
+    }
+}
